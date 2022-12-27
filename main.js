8f2b2d9aba6f48e9a7a60e0673bc66c1c36d590f
dandan200118 <945684725@qq.com>
2022-12-27 19:09:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http')
 var request = require('request');
 
 // 这里填你的replit应用的url，多个url用逗号隔开
-let replit_app_urls = ["https://google.diandianvs.tk"]
+let replit_app_urls = ["https://wordpress.dandan200118.repl.co"]
 
 var port = 4000
 var server = http.createServer(function (request, response) {
